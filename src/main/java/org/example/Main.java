ecdf6c643668b261e3061755e7d681c4dcd9a9b1
Shubha <shubhashreegochhi@gmail.com>
2026-07-10 16:34:27 +0530

    Update in Main function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,27 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String password = "shubha@2610";
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Database Connected Successfully!");
+            Class.forName("com.mysql.cj.jdbc.Driver"); //Step-1
+            DriverManager.getConnection(url,user,password); //Step - 2
 
-        } catch (ClassNotFoundException e) {
+
+
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
 
         }
+        System.out.println("Database Connected Successfully!");
+
     }
 }
